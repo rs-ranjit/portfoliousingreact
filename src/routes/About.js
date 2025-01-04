e5456a5b19d213fb22e2f5769d8ab2f416c13c7f
rs-ranjit <https://github.com/rs-ranjit>
2025-01-04 18:55:20 +0545

    some new features

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const About = () => {
   return (
     <div>
       <Navbar />
+      <HeroImg2  heading="About" text="Im a friendly Front-End Web Developer" />
       <Footer />
     </div>
   )
