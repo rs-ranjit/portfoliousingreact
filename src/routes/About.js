9e7ebd761fe8493d732a373b7553b9abf2e3977e
rs-ranjit <https://github.com/rs-ranjit>
2025-01-06 11:37:07 +0545

    added the about page:

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 const About = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2  heading="About" text="Im a friendly Front-End Web Developer" />
+      <AboutContent />
       <Footer />
     </div>
   )
